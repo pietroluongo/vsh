d25e6094e74650c10dbe0317ed5c9ea3e929375b
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-09-08 22:27:05 -0300

    Added .clang-format to code and updated main code adding basic functionality

--- client/main.c
+++ client/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_COMMAND_SIZE 64
+// clang-format off
+#define EVER ;;
+// clang-format on
+
+int isExitCommand(char* command) { return !strcmp(command, "exit"); }
+
+void rtrim(char* str) {
+    str[strcspn(str, "\n")] = 0;
+}
 
 int main() {
-	printf("all ok...\n");
-	return 0;
+    for (EVER) {
+        printf("vsh> ");
+        char command[MAX_COMMAND_SIZE];
+        fgets(command, MAX_COMMAND_SIZE, stdin);
+        rtrim(command);
+        if (isExitCommand(command)) { break; }
+        printf("got command %s\n", command);
+    }
+    return 0;
 }
