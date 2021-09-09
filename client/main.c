6a34d6622e14ff42e5bf34e651a1aff866b94d31
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-09-08 22:54:49 -0300

    Added debug functions, updated clang to not mess up includes

--- client/main.c
+++ client/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "../include/banned.h"
 
 #define MAX_COMMAND_SIZE 64
@@ -9,8 +10,24 @@
 
 int isExitCommand(char* command) { return !strcmp(command, "exit"); }
 
-void rtrim(char* str) {
-    str[strcspn(str, "\n")] = 0;
+int isDebugCommand(char* command) { return !strcmp(command, "debug"); }
+
+void rtrim(char* str) { str[strcspn(str, "\n")] = 0; }
+
+void bolsonaro() {
+    char* vaccinatedGuy[] = {
+        "\n",
+        "                  _  _\n",
+        "        _ _      (0)(0)-._  _.-'^^'^^'^^'^^'^^'--.\n",
+        "       (.(.)----'`        ^^'                /^   ^^-._\n",
+        "       (    `                 \\             |    _    ^^-._\n",
+        "        VvvvvvvVv~~`__,/.._>  /:/:/:/:/:/:/:/\\  (_..,______^^-.\n",
+        "         `^^^^^^^^`/  /   /  /`^^^^^^^^^>^^>^`>  >        _`)  )\n",
+        "                   (((`   (((`          (((`  (((`        `'--'^\n",
+        "   I feel weird...\n"};
+    for (int i = 0; i < 9; i++) {
+        printf("%s", vaccinatedGuy[i]);
+    }
 }
 
 int main() {
@@ -19,7 +36,12 @@ int main() {
         char command[MAX_COMMAND_SIZE];
         fgets(command, MAX_COMMAND_SIZE, stdin);
         rtrim(command);
-        if (isExitCommand(command)) { break; }
+        if (isExitCommand(command)) {
+            break;
+        }
+        if (isDebugCommand(command)) {
+            bolsonaro();
+        }
         printf("got command %s\n", command);
     }
     return 0;
