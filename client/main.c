3304a6b67037e5e59d2148ed576bf1f12d460a5f
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-09-14 23:28:18 -0300

    Added code to block unwanted signals

--- client/main.c
+++ client/main.c
@@ -1,7 +1,24 @@
 #include "../include/banned.h"
 #include "../include/vsh.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void setBlockedSignals(sigset_t* mask) {
+    sigaddset(mask, SIGINT);
+    sigaddset(mask, SIGSTOP);
+    sigaddset(mask, SIGQUIT);
+    int result = sigprocmask(SIG_SETMASK, mask, NULL);
+    if(result == -1) {
+        printf("Erro ao configurar mascara de processos.\n");
+        exit(1);
+    }
+    return;
+}
 
 int main() {
+    sigset_t blockedSignals;
+    setBlockedSignals(&blockedSignals);
     vsh_mainLoop();
     return 0;
 }
