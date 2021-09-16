b71611d73ed4872d6f124ea5326a50cd618b89a9
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-09-15 22:45:55 -0300

    Updated #defines to better reflect the project specification

--- include/vsh.h
+++ include/vsh.h
@@ -1,8 +1,8 @@
 #ifndef VSH_H
 #define VSH_H
 
-#define MAX_COMMAND_SIZE      4096
-#define MAX_COMMAND_ARGS      64
+#define MAX_COMMAND_SIZE      1024
+#define MAX_COMMAND_ARGS      3
 #define MAX_COMMANDS_PER_LINE 5
 
 void vsh_mainLoop();
