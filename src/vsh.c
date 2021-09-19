972cb31a1372ecd0e2b6ef2ec820ab47a20a6a73
Otávio <otavio@leanonsystems.com>
2021-09-19 19:12:11 -0300

    Implemented the multiple commands feature

--- src/vsh.c
+++ src/vsh.c
@@ -93,8 +93,88 @@ int execForegroundCommand(CommandData* command) {
     }
 }
 
+void closeAllUnusedPipes(int (*fd)[2], int nPipes) {
+    for (int i = 0; i < nPipes; i++) {
+        close(fd[i][0]);
+        close(fd[i][1]);
+    }
+}
+
 int execBackgroundCommands(CommandDataArray* commandList) {
-    printf("TODO: implement execBackgroundCommand()\n");
+    int nCommands = (int)commandList->size;
+    pid_t pid[nCommands];
+    int execStat[nCommands];
+    int nPipes = nCommands - 1;
+    int fd[nPipes][2];
+
+    for(int i = 0; i < nPipes; i++) {
+        if (pipe(fd[i]) < 0) {
+            printf("ERROR creating pipe with index %d\n", i);
+            for(int j = 0; j < i; j++) {
+                close(fd[j][0]);
+                close(fd[j][1]);
+            }
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < nCommands; i++) {
+        CommandData* command = &commandList->data[i];
+        pid[i] = fork();
+        if (pid[i] == -1) {
+                printf("ERROR forking with index %d\n", i);
+                exit(1); 
+        }
+        int wstatus;
+        if (utils_isChildProcess(pid[i])) {
+            if (i == 0) {
+                // first process needs to stdout to the first pipe
+                dup2(fd[i][1], STDOUT_FILENO);
+                closeAllUnusedPipes(fd, nPipes);
+                execStat[i] = execvp(getCommandProgram(command), command->argv);
+                if (execStat < 0) {
+                    printf("Erro executando comando %s\n", getCommandProgram(command));
+                    exit(1);
+                }
+            } else if (i == nCommands - 1) {
+                // end process needs to stdin from previous pipe
+                dup2(fd[i - 1][0], STDIN_FILENO);
+                closeAllUnusedPipes(fd, nPipes);
+                execStat[i] = execvp(getCommandProgram(command), command->argv);
+                if (execStat < 0) {
+                    printf("Erro executando comando %s\n", getCommandProgram(command));
+                    exit(1);
+                }
+            } else {
+                /* middle processes need to stdin from previous pipe
+                 * and stdout to current pipe */ 
+                dup2(fd[i - 1][0], STDIN_FILENO);
+                dup2(fd[i][1], STDOUT_FILENO);
+                closeAllUnusedPipes(fd, nPipes);
+                execStat[i] = execvp(getCommandProgram(command), command->argv);
+                if (execStat < 0) {
+                    printf("Erro executando comando %s\n", getCommandProgram(command));
+                    exit(1);
+                }
+            }
+            freeCommandDataArray(commandList);
+            exit(0);
+        } else {
+            if (i > 0) {
+                if (close(fd[i - 1][0])) {
+                    printf("error closing pipe %d %d\n", i - 1, 0);
+                    exit(1);
+                }
+                if (close(fd[i - 1][1])) {
+                    printf("error closing pipe %d %d\n", i - 1, 0);
+                    exit(1);
+                }
+            }
+            waitpid(pid[i], &wstatus, 0);
+            showProcessExitStatus(wstatus, pid[i]);
+        }
+    }
+    printf("Finished executing background commands\n");
 }
 
 void setBlockedSignals(sigset_t* mask) {
