pietroluongo/vsh

commit 562c0edfc35c80bfce5b76bee8ba972b9b5b88f7

Updated
- Removed inlined getCommandProgram function, as it was causing random headaches
- Removed old implementation of getCommandProgram from vsh in favour of cmd_ version
- Simplified piping logic

include/command.h
@@ -20,9 +20,9 @@ void              cmd_printCreatedCommandData(CommandData* cData);
 CommandDataArray* cmd_buildCommandStructsFromLine(char* line);
 void              cmd_freeCommandDataArray(CommandDataArray* commandData);
 void              cmd_checkStatus(int execStatus, char* command);
-void              cmd_buildCommandForProgramFromString(char*        command,
-                                                       CommandData* commandData);
+char*             cmd_getCommandProgram(CommandData* command);
 
-char* cmd_getCommandProgram(CommandData* command) { return command->argv[0]; };
+void cmd_buildCommandForProgramFromString(char*        command,
+                                          CommandData* commandData);
 
 #endif // VSH_COMMAND_H

src/vsh.c
@@ -37,8 +37,6 @@ void showProcessExitStatus(int wstatus, pid_t childPid) {
     }
 }
 
-char* getCommandProgram(CommandData* command) { return command->argv[0]; }
-
 void readCommandFromStdin(char* whereToStore) {
     fgets(whereToStore, MAX_COMMAND_SIZE, stdin);
     utils_rtrim(whereToStore);
@@ -89,8 +87,8 @@ int execForegroundCommand(CommandData* command) {
     checkForkError(pid);
     int wstatus;
     if (utils_isChildProcess(pid)) {
-        int execStatus = execvp(getCommandProgram(command), command->argv);
-        cmd_checkStatus(execStatus);
+        int execStatus = execvp(cmd_getCommandProgram(command), command->argv);
+        cmd_checkStatus(execStatus, cmd_getCommandProgram(command));
     } else {
         waitpid(pid, &wstatus, 0);
         showProcessExitStatus(wstatus, pid);
@@ -121,27 +119,19 @@ int execBackgroundCommands(CommandDataArray* commandList) {
             if (i == 0) {
                 // first process needs to stdout to the first pipe
                 dup2(pipeDescriptors[i][WRITE], STDOUT_FILENO);
-                utils_closeAllPipes(pipeDescriptors, nPipes);
-                execStatus[i] =
-                    execvp(getCommandProgram(command), command->argv);
-                cmd_checkStatus(execStatus[i], getCommandProgram(command));
             } else if (i == nCommands - 1) {
                 // end process needs to stdin from previous pipe
-                dup2(pipeDescriptors[i - 1][READ], STDIN_FILENO);
-                utils_closeAllPipes(pipeDescriptors, nPipes);
-                execStatus[i] =
-                    execvp(getCommandProgram(command), command->argv);
-                cmd_checkStatus(execStatus[i], getCommandProgram(command));
+                cmd_checkStatus(execStatus[i], cmd_getCommandProgram(command));
             } else {
                 /* middle processes need to stdin from previous pipe
                  * and stdout to current pipe */
                 dup2(pipeDescriptors[i - 1][READ], STDIN_FILENO);
                 dup2(pipeDescriptors[i][WRITE], STDOUT_FILENO);
-                utils_closeAllPipes(pipeDescriptors, nPipes);
-                execStatus[i] =
-                    execvp(getCommandProgram(command), command->argv);
-                cmd_checkStatus(execStatus[i], getCommandProgram(command));
             }
+            execStatus[i] =
+                execvp(cmd_getCommandProgram(command), command->argv);
+            cmd_checkStatus(execStatus[i], cmd_getCommandProgram(command));
+            utils_closeAllPipes(pipeDescriptors, nPipes);
             cmd_freeCommandDataArray(commandList);
             exit(0);
         } else {

src/command.c
@@ -53,8 +53,10 @@ void cmd_freeCommandDataArray(CommandDataArray* commandData) {
 }
 
 void cmd_checkStatus(int execStatus, char* command) {
-    if(execStatus == -1) {
+    if (execStatus == -1) {
         printf("Erro executando comando %s\n", command);
         exit(1);
     }
 }
+
+char* cmd_getCommandProgram(CommandData* command) { return command->argv[0]; };

commit 9fd8c934adb161d0d5a3cf1781ee55f076dafacc

Fixes regressions introduced in "Updated"
This fixes regressions introduced in commit 562c0edfc35c80bfce5b76bee8ba972b9b5b88f7.

include/command.h
@@ -20,9 +20,8 @@ void              cmd_printCreatedCommandData(CommandData* cData);
 CommandDataArray* cmd_buildCommandStructsFromLine(char* line);
 void              cmd_freeCommandDataArray(CommandDataArray* commandData);
 void              cmd_checkStatus(int execStatus, char* command);
-char*             cmd_getCommandProgram(CommandData* command);
+void              cmd_buildCommandForProgramFromString(char*        command,
+                                                       CommandData* commandData);
 
-void cmd_buildCommandForProgramFromString(char*        command,
-                                          CommandData* commandData);
-
+char* cmd_getCommandProgram(CommandData* command);
 #endif // VSH_COMMAND_H

src/vsh.c
@@ -37,6 +37,8 @@ void showProcessExitStatus(int wstatus, pid_t childPid) {
     }
 }
 
+char* getCommandProgram(CommandData* command) { return command->argv[0]; }
+
 void readCommandFromStdin(char* whereToStore) {
     fgets(whereToStore, MAX_COMMAND_SIZE, stdin);
     utils_rtrim(whereToStore);
@@ -87,8 +89,8 @@ int execForegroundCommand(CommandData* command) {
     checkForkError(pid);
     int wstatus;
     if (utils_isChildProcess(pid)) {
-        int execStatus = execvp(cmd_getCommandProgram(command), command->argv);
-        cmd_checkStatus(execStatus, cmd_getCommandProgram(command));
+        int execStatus = execvp(getCommandProgram(command), command->argv);
+        cmd_checkStatus(execStatus, getCommandProgram(command));
     } else {
         waitpid(pid, &wstatus, 0);
         showProcessExitStatus(wstatus, pid);
@@ -128,10 +130,9 @@ int execBackgroundCommands(CommandDataArray* commandList) {
                 dup2(pipeDescriptors[i - 1][READ], STDIN_FILENO);
                 dup2(pipeDescriptors[i][WRITE], STDOUT_FILENO);
             }
-            execStatus[i] =
-                execvp(cmd_getCommandProgram(command), command->argv);
-            cmd_checkStatus(execStatus[i], cmd_getCommandProgram(command));
             utils_closeAllPipes(pipeDescriptors, nPipes);
+            execStatus[i] = execvp(getCommandProgram(command), command->argv);
+            cmd_checkStatus(execStatus[i], getCommandProgram(command));
             cmd_freeCommandDataArray(commandList);
             exit(0);
         } else {
@@ -145,7 +146,6 @@ int execBackgroundCommands(CommandDataArray* commandList) {
                     exit(1);
                 }
             }
-            waitpid(pid[i], &wstatus, 0);
             showProcessExitStatus(wstatus, pid[i]);
         }
     }